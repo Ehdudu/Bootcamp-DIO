842e79e1f8d7127afb51cce01269261e1f145e68
Dudu <edu.ebeng2@gmail.com>
2024-04-02 13:04:00 -0300

    Atualizado para guardar a aceleração em um array global ao invés de localmente.

--- Auxilio-RCP/main/main.ino
+++ Auxilio-RCP/main/main.ino
@@ -1,9 +1,15 @@
 #include<Wire.h>
 const int MPU_addr=0x68; // Accelerometer's I2C address
+const int accCollectionLimit=1000; // Defines a hard cap used for resetting the acceleration array once it reaches its limit. 1000 points = +-25 compressions
+const int compLimit=25; // defines the amount of compressions necessary for the compression frequency to be calculated
 bool compStart=0; // Tracks start of compressions
-float newAcc=0,accTrend=0, velTrend=0; // newAcc stores old acceleration value for comparison with new value, trend is used for detrending
+float acceleration[accCollectionLimit]={}; // acceleration values collected over time.
+float accTrend=0, velTrend=0; // trend is used for detrending
+float compressionDepth=0; // stores depth of compression 
+int i=0; // used for passing values to the acceleration array
 int riseCounter=0,compCounter=0; // riseCounter tracks downwars movement of acceleration, compCounter tracks amount of compressions made
 unsigned long compStartTime=0, compEndTime=0; // compStartTime tracks the instant of time of the first compression, compEndTime tracks the instant of time after a specific number of compressions defined on compressionCount()
+unsigned long time[accCollectionLimit]={}; // acceleration values collected over time.
 double compFreq=0; // compFreq tracks the average amount of compressions per minute 
 
 void setup(){
@@ -19,31 +25,39 @@ void setup(){
   Wire.write(0x10);                  // Defines the measurement scale as +/- 8g
   Wire.endTransmission(true);
 
+  Serial.begin(115200); // Starts serial communication
+  Serial.println("Programa iniciado, 5 segundos até cálculo de trend");
+  delay(5000);
+
   // Measuring the average accelerometer readings during rest.
   int i=0;
   double accSum=0;
   do {
     i++;
     accSum += collectAcc();
-  }while(millis()<5000);
+  }while(millis()<10000);
   accTrend = accSum/i;
 
-  Serial.begin(115200); // Starts serial communication
-  Serial.print(accTrend); // For initial test purposes only
+  Serial.print("Trend calculada:"); Serial.println(accTrend); // For initial test purposes only
 }
 
-void loop(){
-  float acc; // To be substituted by a globally declared vector that holds multiple values
-  acc = collectAcc()-accTrend; // Collects detrended acceleration
-  compressionCount(acc); // Tracks compressions based on collected acceleration values
+void loop(){  
+  acceleration[i]= collectAcc()-accTrend;
+  time[i]=millis();
+  compressionCount(); // Tracks compressions based on collected and detrended acceleration values
+  i++;
+  if(i>=(accCollectionLimit-1)) {
+    i=0;
+    compCounter=0;
+    riseCounter=0;
+    //compressionDepth = depthMeasure();
+  } 
   
   
   //Serial.print(millis()); Serial.print(" ; "); Serial.print(acc); Serial.print(" ; "); Serial.print(compCounter); Serial.print(" ; "); Serial.print(compPeriod); Serial.print(" ; "); Serial.println(compFreq);
   
   delay(7);
 }
-
-
 
 float collectAcc() {
   int16_t rawAcc; // Stores acceleration from the sensor
@@ -60,9 +74,9 @@ float collectAcc() {
   return gAcc;
 }
 
-void compressionCount(float acc) {    
-  float oldAcc = newAcc;
-  newAcc = acc;
+void compressionCount() {
+  float oldAcc = (i>0) ? acceleration[i-1] : acceleration[accCollectionLimit-1]; 
+  float newAcc=acceleration[i];
 
   if(newAcc>oldAcc && riseCounter>3) { //Tracks the lowest point of acceleration
     riseCounter=0;
@@ -70,7 +84,7 @@ void compressionCount(float acc) {
     if(compCounter==1){ // aumentar as condições de compcounter para evitar que no começo do programa ele pegue movimentação e interprete como compressão
       compStartTime=millis();
     }
-    if(compCounter==10){
+    if(compCounter==compLimit){
       compEndTime=millis();
       compCounter=0;
       compStart=1;
@@ -82,4 +96,8 @@ void compressionCount(float acc) {
   if(newAcc<oldAcc && abs(newAcc)>1) { // Tracks downwards movement as long as the acceleration is higher than 1m/s²
     riseCounter++;
   }
+}
+
+float depthMeasure() {
+
 }
