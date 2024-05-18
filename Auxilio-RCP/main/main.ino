a2f3faae346a12fad6ccdc05048be178fa98282d
Marshmelio <edu.ebeng2@gmail.com>
2024-05-18 15:33:09 -0300

    Alguns pequenos erros corrigidos, porém é importante ressaltar que o programa terá de se refeito agora visto erro encontrado na testagem prática

--- Auxilio-RCP/main/main.ino
+++ Auxilio-RCP/main/main.ino
@@ -29,26 +29,32 @@ void setup(){
   Serial.begin(115200); // Starts serial communication
   Serial.println("Por favor, coloque o acelerometro virado para baixo. O calculo  de trend se inicia em 5 segundos");
   delay(5000);
-
+  
   unsigned long timeDelta[500]={};
   float acc5PointMovingAvg[500]={}, accOffset[500]={}, velocity[500]={};
 
   for(int i=0;i<500;i++) {
-    acceleration[i]= collectAcc();
+    acceleration[i]=collectAcc();
     collectAccTime[i]=millis();
   }
-
-  movingAverage(acc5PointMovingAvg,acceleration,5); // Calculated a moving average of the acceleration in order to reduce noise
+  
+  movingAverage(acc5PointMovingAvg,acceleration,5,500); // Calculated a moving average of the acceleration in order to reduce noise
+  
   accTrend = trendCalc(500,acc5PointMovingAvg); // Calculates the noise trend before compressions start. 100 is an arbitrary value, used so to make sure that the first slope decline isn't included in the trend calculation.
   for(int i=1;i<500;i++) {
     timeDelta[i]=collectAccTime[i]-collectAccTime[i-1]; // Calculates time between measurements
     accOffset[i]=acc5PointMovingAvg[i]-accTrend; // detrends acceleration
   }
-  integralCalculator(velocity,accOffset,timeDelta); // Calculates the velocity, which is the integral of the acceleration
+  delay(5000);
+  for(int i=0;i<500;i++) {
+    Serial.println(acceleration[i]);
+    Serial.println(i);
+  }
+      
+  integralCalculator(velocity,accOffset,timeDelta,500); // Calculates the velocity, which is the integral of the acceleration
   velTrend=trendCalc(500,velocity); // Calculates the noise trend in the velocity.
-
   Serial.println("Trend calculada."); Serial.println("Aceleracao: ."); Serial.println(accTrend); Serial.println("\nVelocidade: ."); Serial.println(velTrend);
-  Serial.println("\n\nInicie as compressoes");
+  Serial.println("\n\nInicie as compressoes"); 
   delay(5000);
 }
 
@@ -64,10 +70,10 @@ void loop(){
     compCounter=0;
     riseCounter=0;
   }
-  Serial.print(millis()); Serial.print(" ; ");
-  Serial.print(acceleration[measurementCounter]); Serial.print(" ; ");
-  Serial.print(compFreq); Serial.print(" ; ");
-  Serial.print(compressionDepth); Serial.print(" ; ");
+  //Serial.print(millis()); Serial.print(" ; ");
+  //Serial.print(acceleration[measurementCounter]); Serial.print(" ; ");
+  //Serial.print(compFreq); Serial.print(" ; ");
+  //Serial.print(compressionDepth); Serial.print(" ; ");
 
   measurementCounter++;
   delay(7);
@@ -116,20 +122,20 @@ float depthMeasure() {
   float amplitude[accCollectionLimit]={}, avgAmplitude[accCollectionLimit]={};
 
 
-  movingAverage(acc5PointMovingAvg,acceleration,5); // Calculated a moving average of the acceleration in order to reduce noise
+  movingAverage(acc5PointMovingAvg,acceleration,5,accCollectionLimit); // Calculated a moving average of the acceleration in order to reduce noise
   
   for(int i=1;i<accCollectionLimit;i++) {
     timeDelta[i]=collectAccTime[i]-collectAccTime[i-1]; // Calculates time between measurements
     accOffset[i]=acc5PointMovingAvg[i]-accTrend; // detrends acceleration
   }
-  integralCalculator(velocity,accOffset,timeDelta); // Calculates the velocity, which is the integral of the acceleration
+  integralCalculator(velocity,accOffset,timeDelta,accCollectionLimit); // Calculates the velocity, which is the integral of the acceleration
 
   for(int i=1;i<accCollectionLimit;i++) {
     velOffset[i]=velocity[i]-velTrend; // detrends velocity
   }
 
-  integralCalculator(displacement,velOffset,timeDelta); // Calculates displacement, which is the integral of the velocity
-  movingAverage(disp121PointMovingAvg,displacement,121);
+  integralCalculator(displacement,velOffset,timeDelta,accCollectionLimit); // Calculates displacement, which is the integral of the velocity
+  movingAverage(disp121PointMovingAvg,displacement,121,accCollectionLimit);
   for(int i=0;i<accCollectionLimit;i++) {
     dispInCentimeters[i]=(displacement[i]-disp121PointMovingAvg[i])*100;
   }
@@ -139,7 +145,7 @@ float depthMeasure() {
     amplitude[i]=maxValue[i]-minValue[i];
   }
 
-  movingAverage(avgAmplitude,amplitude,250);
+  movingAverage(avgAmplitude,amplitude,250,accCollectionLimit);
   double sum=0;
   for(int i=150;i<=(accCollectionLimit-250);i++) { // this is calculating the average amplitude, but only of the points affected by the moving average. Technically, I could've used 125 instead of 150, but i decided to shoot higher for a safe margin
     sum += avgAmplitude[i];
@@ -174,17 +180,16 @@ float trendCalc(int stoppingMeasurement, float *measurement) {
   return trend;
 }
 
-void integralCalculator(float *integral, float *data, unsigned long *period) {
-  int stoppingMeasurement = accCollectionLimit;
+void integralCalculator(float *integral, float *data, unsigned long *period, int lastNumber) {
+  int stoppingMeasurement = lastNumber;
 
-  integral[stoppingMeasurement]={};
   for(int i=1;i<stoppingMeasurement;i++) {
     integral[i]=((data[i-1]+data[i])*((float)period[i]/1000)/2)+integral[i-1];
   }
 }
 
-void movingAverage(float *avg,float *data,int period) {
-  int stoppingMeasurement = accCollectionLimit;
+void movingAverage(float *avg,float *data,int period, int lastNumber) {
+  int stoppingMeasurement = lastNumber;
   int halfPeriod = (period-1)/2;
 
   for(int i=0;i<stoppingMeasurement;i++) {
@@ -194,6 +199,7 @@ void movingAverage(float *avg,float *data,int period) {
     }
     avg[i]= (i+halfPeriod>stoppingMeasurement || i-halfPeriod<0) ? data[i] : sum/period;
   }
+
 }
 
 void getMax(float *maximum, float *data,int period) {
